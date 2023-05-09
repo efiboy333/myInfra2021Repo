0713c6d84d36aad35356331076a5690c21cdbe9d
Efiboy <119268312+efiboy333@users.noreply.github.com>
2023-05-09 15:59:54 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "launch-wizard-7" 
+    default     = "jenkins-sgroup-dec-2021" 
 }
 
 variable "tag_name" { 
