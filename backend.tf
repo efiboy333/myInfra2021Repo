3d49150e2f43d63ab31dc5e2355a4727904f1215
Efiboy <119268312+efiboy333@users.noreply.github.com>
2023-11-29 14:36:43 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "i-am-rich-and-wealthy"
+    bucket = "victory"
     key = "main"
     region = "eu-west-2"
     dynamodb_table = "we-are-rich-and-wealthy"
